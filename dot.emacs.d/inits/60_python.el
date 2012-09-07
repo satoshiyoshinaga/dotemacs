3f9a436d4a4a3016cf093f1d8e9c0940271aa936
Satoshi Yoshinaga (iRidge) <satoshi.yoshinaga@iridge.jp>
2012-09-07 18:06:26 +0900

    pythonの設定

---
+++ dot.emacs.d/inits/60_python.el
@@ -0,0 +1,15 @@
+;;; flymake for python
+(add-hook 'find-file-hook 'flymake-find-file-hook)
+(when (load "flymake" t)
+  (defun flymake-pyflakes-init ()
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+           (local-file (file-relative-name
+                        temp-file
+                        (file-name-directory buffer-file-name))))
+      (list "pychecker"  (list local-file))))
+  (add-to-list 'flymake-allowed-file-name-masks
+               '("\\.py\\'" flymake-pyflakes-init)))
+(load-library "flymake-cursor")
+(global-set-key [f10] 'flymake-goto-prev-error)
+(global-set-key [f11] 'flymake-goto-next-error)
