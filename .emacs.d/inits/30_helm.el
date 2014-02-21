c8675098682c1d200c572cc072e4ed0f8f2ab471
Satoshi Yoshinaga <satoshi.yoshinaga.147@gmail.com>
2014-02-21 14:58:28 +0900

    C-h at helm-mode

--- .emacs.d/inits/30_helm.el
+++ .emacs.d/inits/30_helm.el
@@ -1,5 +1,10 @@
 (require 'helm-config)
+(helm-mode 1)
 (global-set-key (kbd "C-;")     'helm-mini)
 (global-set-key (kbd "C-x C-f")     'helm-find-files)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (global-set-key (kbd "M-x") 'helm-M-x)
+
+;; C-h at minibuffer
+(define-key helm-map (kbd "C-h") 'delete-backward-char)
+(define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
