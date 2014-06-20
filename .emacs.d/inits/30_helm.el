48d017a23d3caa67432baaf30e7eca8365839d5e
Satoshi Yoshinaga <satoshi.yoshinaga.147@gmail.com>
2014-06-20 20:37:52 +0900

    use TAB for execute helm.

--- .emacs.d/inits/30_helm.el
+++ .emacs.d/inits/30_helm.el
@@ -8,3 +8,5 @@
 ;; C-h at minibuffer
 (define-key helm-map (kbd "C-h") 'delete-backward-char)
 (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
+(define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
+(define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
