bf6e66f586107bca0579a0971897787f3913ec08
Satoshi Yoshinaga @MBA <satoshi.yoshinaga.147@gmail.com>
2014-09-01 13:24:25 +0900

    helm-alcで楽々翻訳

--- .emacs.d/inits/30_helm.el
+++ .emacs.d/inits/30_helm.el
@@ -10,3 +10,28 @@
 (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
 (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
 (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+
+;; alc
+(defun helm-alc:init ()
+  (with-current-buffer (helm-candidate-buffer 'global)
+    (let ((ret (call-process-shell-command "look ." nil t)))
+      (unless (zerop ret)
+        (error "Failed look ")))))
+
+(defun helm-alc:action (word)
+  (loop for marked in (helm-marked-candidates)
+        for url = (format "http://eow.alc.co.jp/%s/UTF-8/?ref=sa" marked)
+        do
+        (browse-url url)))
+
+(defvar helm-c-source-alc
+  '((name . "alc")
+    (init . helm-alc:init)
+    (candidates-in-buffer)
+    (candidate-number-limit . 100)
+    (action . helm-alc:action)
+    (requires-pattern . 2)))
+
+(defun helm-alc ()
+  (interactive)
+  (helm :sources '(helm-c-source-alc) :buffer "*helm alc*"))
