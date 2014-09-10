6d3d4cadb272a9b2425cbcc30ed9060a09e5ebb8
Satoshi Yoshinaga @MBA <satoshi.yoshinaga.147@gmail.com>
2014-09-10 13:45:42 +0900

    マジックコメントの自動挿入をやめる

--- .emacs.d/inits/60_ruby.el
+++ .emacs.d/inits/60_ruby.el
@@ -3,3 +3,7 @@
 (add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+
+(setq ruby-insert-encoding-magic-comment nil)
+
+
